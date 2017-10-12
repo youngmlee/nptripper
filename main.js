0e94cf1feceec20aa207aebaa57e50ce36c200af
Young Lee <ylee8575@gmail.com>
2017-10-12 09:52:00 -0700

    Added park carousel images to images folder and created bootstrap carousel.#2

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 var map;
-var src='http://gdurl.com/ba_Lf'
-
-const $descript = document.querySelector('#capture')
+var src='http://gdurl.com/NiZm'
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -23,3 +21,12 @@ function initMap() {
     testimonial.innerHTML = content;
   });
 }
+
+const $users = document.getElementById('capture')
+const url = 'https://developer.nps.gov/api/v0/parks?parkCode=yell'
+
+fetch(url, {headers:{AUTHORIZATION: '0CA48841-5F16-430F-BB39-F9AD09242D77'}})
+  .then(response => {
+    return response.json()
+  })
+  .then(console.log('something fetched! where is it?'))

011b3f37a58609a7963be9ce550dc3f86ea380b9
Young Lee <ylee8575@gmail.com>
2017-10-19 14:54:54 -0700

    Created park preview lightboxes in description panel on submit. #4

--- main.js
+++ main.js
@@ -218,6 +218,15 @@ function findObjByParkCode (parkCode, array) {
   }
 }
 
+function findObjByParkName (parkName, array) {
+  for (var i = 0; i < array.length; i++) {
+    var arrObj = array[i]
+    if (parkName === arrObj.name) {
+      return arrObj
+    }
+  }
+}
+
 function popCar(parkCode) {
   var $carousel = document.querySelectorAll('.carousel-inner img')
   var foundObj = findObjByParkCode(parkCode, parkInfo)
@@ -339,7 +348,9 @@ function getOrgDest () {
 }
 
 function calcDistance() {
+  var parkList = []
   var service = new google.maps.DistanceMatrixService;
+  var outputDiv = document.querySelector('.description');
   service.getDistanceMatrix({
     origins: getOrgDest(),
     destinations: topParks,
@@ -361,16 +372,58 @@ function calcDistance() {
       for (var j = 0; j < results.length; j++) {
         if (+results[j].distance.text.slice(0, -3) <= 50) {
           outputDiv.innerHTML += 'The distance from ' + originList[i] + ' to ' + destinationList[j] + ' is a mere ' + results[j].distance.text + ' which will take approximately ' + results[j].duration.text + '. A stone\'s throw away!'+ '<br><br>';
+          parkList.push(destinationList[j])
         } else if (+results[j].distance.text.slice(0, -3) <= 100) {
           outputDiv.innerHTML += 'The distance from ' + originList[i] + ' to ' + destinationList[j] + ' is a mere ' + results[j].distance.text + ' which will take approximately ' + results[j].duration.text + '. A trifling distance!'+ '<br><br>';
+          parkList.push(destinationList[j])
         } else if (+results[j].distance.text.slice(0, -3) <= 150) {
           outputDiv.innerHTML += 'The distance from ' + originList[i] + ' to ' + destinationList[j] + ' is a mere ' + results[j].distance.text + ' which will take approximately ' + results[j].duration.text + '. Well worth the detour!'+ '<br><br>';
+          parkList.push(destinationList[j])
         } else if (+results[j].distance.text.slice(0, -3) <= 200) {
           outputDiv.innerHTML += 'The distance from ' + originList[i] + ' to ' + destinationList[j] + ' is a mere ' + results[j].distance.text + ' which will take approximately ' + results[j].duration.text + '. A real adventure!'+ '<br><br>';
+          parkList.push(destinationList[j])
         }
       }
     }
+    createGalleries(parkList)
   })
+}
+
+function createGalleries(arr) {
+  const outputDiv = document.querySelector('.description')
+  const $testingArea = document.querySelector('.testingarea')
+  for (let i = 0; i < arr.length; i++) {
+    var singlePark = arr[i]
+    var foundPark = findObjByParkName(singlePark, parkInfo)
+    const $lightboxGallery = document.createElement('div')
+    $lightboxGallery.setAttribute('class', 'gallery')
+    const $a1 = document.createElement('a')
+    $a1.setAttribute('href', foundPark.src[0])
+    $a1.innerText = foundPark.name + " park preview"
+    const $a2 = document.createElement('a')
+    $a2.setAttribute('href', foundPark.src[1])
+    const $a3 = document.createElement('a')
+    $a3.setAttribute('href', foundPark.src[2])
+    const $a4 = document.createElement('a')
+    $a4.setAttribute('href', foundPark.src[3])
+    const $a5 = document.createElement('a')
+    $a5.setAttribute('href', foundPark.src[4])
+    $lightboxGallery.appendChild($a1)
+    $lightboxGallery.appendChild($a2)
+    $lightboxGallery.appendChild($a3)
+    $lightboxGallery.appendChild($a4)
+    $lightboxGallery.appendChild($a5)
+    outputDiv.appendChild($lightboxGallery)
+  }
+  $('.gallery').each(function() {
+    $(this).magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      gallery: {
+        enabled:true
+      }
+    });
+  });
 }
 
 const topParks = [
@@ -425,7 +478,7 @@ const neglectedParks = [
 const parkInfo = [
   {
     id: '0',
-    name: 'Acadia National Park',
+    name: 'Acadia National Park, Maine, USA',
     parkCode: 'acad',
     src: ['images/acad/acad1.jpg', 'images/acad/acad2.jpg', 'images/acad/acad3.jpg', 'images/acad/acad4.jpg', 'images/acad/acad5.jpg'],
     LatLng: [ 44.3385559, -68.2733346 ],
@@ -434,7 +487,7 @@ const parkInfo = [
   },
   {
     id: '1',
-    name: 'Arches National Park',
+    name: 'Arches National Park, Utah, USA',
     parkCode: 'arch',
     src: ['images/arch/arch1.jpg', 'images/arch/arch2.jpg', 'images/arch/arch3.jpg', 'images/arch/arch4.jpg', 'images/arch/arch5.jpg'],
     LatLng: [ 38.6165441, -109.6163418 ],
@@ -443,7 +496,7 @@ const parkInfo = [
   },
   {
     id: '2',
-    name: 'Badlands National Park',
+    name: 'Badlands National Park, South Dakota, USA',
     parkCode: 'badl',
     src: ['images/badl/badl1.jpg', 'images/badl/badl2.jpg', 'images/badl/badl3.jpg', 'images/badl/badl4.jpg', 'images/badl/badl5.jpg'],
     LatLng: [ 43.8553804, -102.3396912 ],
@@ -452,7 +505,7 @@ const parkInfo = [
   },
   {
     id: '3',
-    name: 'Big Bend National Park',
+    name: 'Big Bend National Park, Texas, USA',
     parkCode: 'bibe',
     src: ['images/bibe/bibe1.jpg', 'images/bibe/bibe2.jpg', 'images/bibe/bibe3.jpg', 'images/bibe/bibe4.jpg', 'images/bibe/bibe5.jpg'],
     LatLng: [ 29.1274869, -103.2425379 ],
@@ -461,7 +514,7 @@ const parkInfo = [
   },
   {
     id: '4',
-    name: 'Biscayne, FL',
+    name: 'Biscayne, Florida, USA',
     parkCode: 'bisc',
     src: ['images/bisc/bisc1.jpg', 'images/bisc/bisc2.jpg', 'images/bisc/bisc3.jpg', 'images/bisc/bisc4.jpg', 'images/bisc/bisc5.jpg'],
     LatLng: [ 25.4630212, -80.3463698 ],
@@ -470,7 +523,7 @@ const parkInfo = [
   },
   {
     id: '5',
-    name: 'Black Canyon of the Gunnison National Park',
+    name: 'Black Canyon of the Gunnison National Park, Colorado, USA',
     parkCode: 'blca',
     src: ['images/blca/blca1.jpg', 'images/blca/blca2.jpg', 'images/blca/blca3.jpg', 'images/blca/blca4.jpg', 'images/blca/blca5.jpg'],
     LatLng: [ 38.5753936, -107.7415961 ],
@@ -479,7 +532,7 @@ const parkInfo = [
   },
   {
     id: '6',
-    name: 'Bryce Canyon National Park',
+    name: 'Bryce Canyon National Park, Utah, USA',
     parkCode: 'brca',
     src: ['images/brca/brca1.jpg', 'images/brca/brca2.jpg', 'images/brca/brca3.jpg', 'images/brca/brca4.jpg', 'images/brca/brca5.jpg'],
     LatLng: [ 37.5930377, -112.1870895 ],
@@ -488,7 +541,7 @@ const parkInfo = [
   },
   {
     id: '7',
-    name: 'Canyonlands National Park',
+    name: 'Canyonlands National Park, Utah, USA',
     parkCode: 'cany',
     src: ['images/cany/cany1.jpg', 'images/cany/cany2.jpg', 'images/cany/cany3.jpg', 'images/cany/cany4.jpg', 'images/cany/cany5.jpg'],
     LatLng: [ 38.3268693, -109.8782592 ],
@@ -497,7 +550,7 @@ const parkInfo = [
   },
   {
     id: '8',
-    name: 'Capitol Reef National Park',
+    name: 'Capitol Reef National Park, Utah, USA',
     parkCode: 'care',
     src: ['images/care/care1.jpg', 'images/care/care2.jpg', 'images/care/care3.jpg', 'images/care/care4.jpg', 'images/care/care5.jpg'],
     LatLng: [ 38.3669703, -111.2615042 ],
@@ -506,7 +559,7 @@ const parkInfo = [
   },
   {
     id: '9',
-    name: 'Carlsbad Caverns National Park',
+    name: 'Carlsbad Caverns National Park, New Mexico, USA',
     parkCode: 'cave',
     src: ['images/cave/cave1.jpg', 'images/cave/cave2.jpg', 'images/cave/cave3.jpg', 'images/cave/cave4.jpg', 'images/cave/cave5.jpg'],
     LatLng: [ 32.1478553, -104.5567138 ],
@@ -515,7 +568,7 @@ const parkInfo = [
   },
   {
     id: '10',
-    name: 'Congaree National Park',
+    name: 'Congaree National Park, South Carolina, USA',
     parkCode: 'cong',
     src: ['images/cong/cong1.jpg', 'images/cong/cong2.jpg', 'images/cong/cong3.jpg', 'images/cong/cong4.jpg', 'images/cong/cong5.jpg'],
     LatLng: [ 33.7916877, -80.7693495 ],
@@ -524,7 +577,7 @@ const parkInfo = [
   },
   {
     id: '11',
-    name: 'Crater Lake National Park',
+    name: 'Crater Lake National Park, Oregon, USA',
     parkCode: 'crla',
     src: ['images/crla/crla1.jpg', 'images/crla/crla2.jpg', 'images/crla/crla3.jpg', 'images/crla/crla4.jpg', 'images/crla/crla5.jpg'],
     LatLng: [ 42.8684411, -122.1684785 ],
@@ -533,7 +586,7 @@ const parkInfo = [
   },
   {
     id: '12',
-    name: 'Cuyahoga Valley National Park',
+    name: 'Cuyahoga Valley National Park, Ohio, USA',
     parkCode: 'cuva',
     src: ['images/cuva/cuva1.jpg', 'images/cuva/cuva2.jpg', 'images/cuva/cuva3.jpg', 'images/cuva/cuva4.jpg', 'images/cuva/cuva5.jpg'],
     LatLng: [ 41.2808248, -81.567812 ],
@@ -542,7 +595,7 @@ const parkInfo = [
   },
   {
     id: '13',
-    name: 'Death Valley, CA',
+    name: 'Death Valley, CA, USA',
     parkCode: 'deva',
     src: ['images/deva/deva1.jpg', 'images/deva/deva2.jpg', 'images/deva/deva3.jpg', 'images/deva/deva4.jpg', 'images/deva/deva5.jpg'],
     LatLng: [ 36.5053891, -117.0794078 ],
@@ -551,7 +604,7 @@ const parkInfo = [
   },
   {
     id: '14',
-    name: 'Everglades, FL',
+    name: 'Everglades, Florida, USA',
     parkCode: 'ever',
     src: ['images/ever/ever1.jpg', 'images/ever/ever2.jpg', 'images/ever/ever3.jpg', 'images/ever/ever4.jpg', 'images/ever/ever5.jpg'],
     LatLng: [ 25.2866149, -80.8986502 ],
@@ -560,7 +613,7 @@ const parkInfo = [
   },
   {
     id: '15',
-    name: 'Glacier National Park',
+    name: 'Glacier National Park, Montana, USA',
     parkCode: 'glac',
     src: ['images/glac/glac1.jpg', 'images/glac/glac2.jpg', 'images/glac/glac3.jpg', 'images/glac/glac4.jpg', 'images/glac/glac5.jpg'],
     LatLng: [ 48.7596128, -113.7870225 ],
@@ -569,7 +622,7 @@ const parkInfo = [
   },
   {
     id: '16',
-    name: 'Grand Canyon National Park',
+    name: 'Grand Canyon National Park, Arizona, USA',
     parkCode: 'grca',
     src: ['images/grca/grca1.jpg', 'images/grca/grca2.jpg', 'images/grca/grca3.jpg', 'images/grca/grca4.jpg', 'images/grca/grca5.jpg'],
     LatLng: [ 36.1069642, -112.1129972 ],
@@ -578,7 +631,7 @@ const parkInfo = [
   },
   {
     id: '17',
-    name: 'Grand Teton National Park',
+    name: 'Grand Teton National Park, Wyoming, USA',
     parkCode: 'grte',
     src: ['images/grte/grte1.jpg', 'images/grte/grte2.jpg', 'images/grte/grte3.jpg', 'images/grte/grte4.jpg', 'images/grte/grte5.jpg'],
     LatLng: [ 43.7904282, -110.6817627 ],
@@ -587,7 +640,7 @@ const parkInfo = [
   },
   {
     id: '18',
-    name: 'Great Basin National Park',
+    name: 'Great Basin National Park, Nevada, USA',
     parkCode: 'grba',
     src: ['images/grba/grba1.jpg', 'images/grba/grba2.jpg', 'images/grba/grba3.jpg', 'images/grba/grba4.jpg', 'images/grba/grba5.jpg'],
     LatLng: [ 38.983333, -114.3 ],
@@ -596,7 +649,7 @@ const parkInfo = [
   },
   {
     id: '19',
-    name: 'Great Sand Dunes National Park',
+    name: 'Great Sand Dunes National Park, Colorado, USA',
     parkCode: 'grsa',
     src: ['images/grsa/grsa1.jpg', 'images/grsa/grsa2.jpg', 'images/grsa/grsa3.jpg', 'images/grsa/grsa4.jpg', 'images/grsa/grsa5.jpg'],
     LatLng: [ 37.7915959, -105.5943276 ],
@@ -605,7 +658,7 @@ const parkInfo = [
   },
   {
     id: '20',
-    name: 'Great Smoky Mountains National Park',
+    name: 'Great Smoky Mountains National Park, United States',
     parkCode: 'grsm',
     src: ['images/grsm/grsm1.jpg', 'images/grsm/grsm2.jpg', 'images/grsm/grsm3.jpg', 'images/grsm/grsm4.jpg', 'images/grsm/grsm5.jpg'],
     LatLng: [ 35.6117644, -83.4895449 ],
@@ -614,7 +667,7 @@ const parkInfo = [
   },
   {
     id: '21',
-    name: 'Guadalupe Mountains National Park',
+    name: 'Guadalupe Mountains National Park, Texas, USA',
     parkCode: 'gumo',
     src: ['images/gumo/gumo1.jpg', 'images/gumo/gumo2.jpg', 'images/gumo/gumo3.jpg', 'images/gumo/gumo4.jpg', 'images/gumo/gumo5.jpg'],
     LatLng: [ 31.94519, -104.8725221 ],
@@ -623,7 +676,7 @@ const parkInfo = [
   },
   {
     id: '22',
-    name: 'Hot Springs National Park',
+    name: 'Hot Springs National Park, 369 central avenue, Hot Springs, AR 71901, USA',
     parkCode: 'hosp',
     src: ['images/hosp/hosp1.jpg', 'images/hosp/hosp2.jpg', 'images/hosp/hosp3.jpg', 'images/hosp/hosp4.jpg', 'images/hosp/hosp5.jpg'],
     LatLng: [ 34.5216915, -93.0423545 ],
@@ -632,7 +685,7 @@ const parkInfo = [
   },
   {
     id: '23',
-    name: 'Joshua Tree National Park',
+    name: 'Joshua Tree National Park, California, USA',
     parkCode: 'jotr',
     src: ['images/jotr/jotr1.jpg', 'images/jotr/jotr2.jpg', 'images/jotr/jotr3.jpg', 'images/jotr/jotr4.jpg', 'images/jotr/jotr5.jpg'],
     LatLng: [ 33.873415, -115.9009923 ],
@@ -641,7 +694,7 @@ const parkInfo = [
   },
   {
     id: '24',
-    name: 'Lassen Volcanic National Park',
+    name: 'Lassen Volcanic National Park, California, USA',
     parkCode: 'lavo',
     src: ['images/lavo/lavo1.jpg', 'images/lavo/lavo2.jpg', 'images/lavo/lavo3.jpg', 'images/lavo/lavo4.jpg', 'images/lavo/lavo5.jpg'],
     LatLng: [ 40.49766, -121.4206552 ],
@@ -650,7 +703,7 @@ const parkInfo = [
   },
   {
     id: '25',
-    name: 'Mammoth Cave National Park',
+    name: 'Mammoth Cave National Park, Kentucky, USA',
     parkCode: 'maca',
     src: ['images/maca/maca1.jpg', 'images/maca/maca2.jpg', 'images/maca/maca3.jpg', 'images/maca/maca4.jpg', 'images/maca/maca5.jpg'],
     LatLng: [ 37.1869978, -86.100528 ],
@@ -659,7 +712,7 @@ const parkInfo = [
   },
   {
     id: '26',
-    name: 'Mesa Verde National Park',
+    name: 'Mesa Verde National Park, Colorado, USA',
     parkCode: 'meve',
     src: ['images/meve/meve1.jpg', 'images/meve/meve2.jpg', 'images/meve/meve3.jpg', 'images/meve/meve4.jpg', 'images/meve/meve5.jpg'],
     LatLng: [ 37.2308729, -108.4618335 ],
@@ -668,7 +721,7 @@ const parkInfo = [
   },
   {
     id: '27',
-    name: 'Mount Rainier National Park',
+    name: 'Mount Rainier National Park, Washington, USA',
     parkCode: 'mora',
     src: ['images/mora/mora1.jpg', 'images/mora/mora2.jpg', 'images/mora/mora3.jpg', 'images/mora/mora4.jpg', 'images/mora/mora5.jpg'],
     LatLng: [ 46.8799663, -121.7269094 ],
@@ -677,7 +730,7 @@ const parkInfo = [
   },
   {
     id: '28',
-    name: 'North Cascades National Park',
+    name: 'North Cascades National Park, Washington, USA',
     parkCode: 'noca',
     src: ['images/noca/noca1.jpg', 'images/noca/noca2.jpg', 'images/noca/noca3.jpg', 'images/noca/noca4.jpg', 'images/noca/noca5.jpg'],
     LatLng: [ 48.7718174, -121.2984648 ],
@@ -686,7 +739,7 @@ const parkInfo = [
   },
   {
     id: '29',
-    name: 'Olympic National Park',
+    name: 'Olympic National Park, 3002 Mt Angeles Rd, Port Angeles, WA 98362, USA',
     parkCode: 'olym',
     src: ['images/olym/olym1.jpg', 'images/olym/olym2.jpg', 'images/olym/olym3.jpg', 'images/olym/olym4.jpg', 'images/olym/olym5.jpg'],
     LatLng: [ 47.8021067, -123.6043524 ],
@@ -695,7 +748,7 @@ const parkInfo = [
   },
   {
     id: '30',
-    name: 'Petrified Forest National Park',
+    name: 'Petrified Forest National Park, Arizona, USA',
     parkCode: 'pefo',
     src: ['images/pefo/pefo1.jpg', 'images/pefo/pefo2.jpg', 'images/pefo/pefo3.jpg', 'images/pefo/pefo4.jpg', 'images/pefo/pefo5.jpg'],
     LatLng: [ 35.065931, -109.78204 ],
@@ -704,7 +757,7 @@ const parkInfo = [
   },
   {
     id: '31',
-    name: 'Pinnacles National Park',
+    name: 'Pinnacles National Park, California, USA',
     parkCode: 'pinn',
     src: ['images/pinn/pinn1.jpg', 'images/pinn/pinn2.jpg', 'images/pinn/pinn3.jpg', 'images/pinn/pinn4.jpg', 'images/pinn/pinn5.jpg'],
     LatLng: [ 36.4905655, -121.1824925 ],
@@ -713,7 +766,7 @@ const parkInfo = [
   },
   {
     id: '32',
-    name: 'Redwood National Park',
+    name: 'Redwood National Park, California, USA',
     parkCode: 'redw',
     src: ['images/redw/redw1.jpg', 'images/redw/redw2.jpg', 'images/redw/redw3.jpg', 'images/redw/redw4.jpg', 'images/redw/redw5.jpg'],
     LatLng: [ 41.2131788, -124.0046275 ],
@@ -722,7 +775,7 @@ const parkInfo = [
   },
   {
     id: '33',
-    name: 'Rocky Mountain National Park',
+    name: 'Rocky Mountain National Park, Colorado, USA',
     parkCode: 'romo',
     src: ['images/romo/romo1.jpg', 'images/romo/romo2.jpg', 'images/romo/romo3.jpg', 'images/romo/romo4.jpg', 'images/romo/romo5.jpg'],
     LatLng: [ 40.3427932, -105.6836389 ],
@@ -731,7 +784,7 @@ const parkInfo = [
   },
   {
     id: '34',
-    name: 'Saguaro National Park',
+    name: 'Saguaro National Park, Arizona, USA',
     parkCode: 'sagu',
     src: ['images/sagu/sagu1.jpg', 'images/sagu/sagu2.jpg', 'images/sagu/sagu3.jpg', 'images/sagu/sagu4.jpg', 'images/sagu/sagu5.jpg'],
     LatLng: [ 32.2967359, -111.166615 ],
@@ -740,7 +793,7 @@ const parkInfo = [
   },
   {
     id: '35',
-    name: 'Sequoia National Park',
+    name: 'Sequoia National Park, California, USA',
     parkCode: 'seki',
     src: ['images/seki/seki1.jpg', 'images/seki/seki2.jpg', 'images/seki/seki3.jpg', 'images/seki/seki4.jpg', 'images/seki/seki5.jpg'],
     LatLng: [ 36.4863668, -118.5657516 ],
@@ -749,7 +802,7 @@ const parkInfo = [
   },
   {
     id: '36',
-    name: 'Shenandoah National Park',
+    name: 'Shenandoah National Park, Virginia, USA',
     parkCode: 'shen',
     src: ['images/shen/shen1.jpg', 'images/shen/shen2.jpg', 'images/shen/shen3.jpg', 'images/shen/shen4.jpg', 'images/shen/shen5.jpg'],
     LatLng: [ 38.6632309, -78.3720764 ],
@@ -758,7 +811,7 @@ const parkInfo = [
   },
   {
     id: '37',
-    name: 'Theodore Roosevelt National Park',
+    name: 'Theodore Roosevelt National Park, North Dakota, USA',
     parkCode: 'this',
     src: ['images/this/this1.jpg', 'images/this/this2.jpg', 'images/this/this3.jpg', 'images/this/this4.jpg', 'images/this/this5.jpg'],
     LatLng: [ 46.978965, -103.5387091 ],
@@ -767,7 +820,7 @@ const parkInfo = [
   },
   {
     id: '38',
-    name: 'Voyageurs National Park',
+    name: 'Voyageurs National Park, Minnesota, USA',
     parkCode: 'voya',
     src: ['images/voya/voya1.jpg', 'images/voya/voya2.jpg', 'images/voya/voya3.jpg', 'images/voya/voya4.jpg', 'images/voya/voya5.jpg'],
     LatLng: [ 48.4268554, -92.8465541 ],
@@ -776,7 +829,7 @@ const parkInfo = [
   },
   {
     id: '39',
-    name: 'Wind Cave National Park',
+    name: 'Wind Cave National Park, South Dakota, USA',
     parkCode: 'wica',
     src: ['images/wica/wica1.jpg', 'images/wica/wica2.jpg', 'images/wica/wica3.jpg', 'images/wica/wica4.jpg', 'images/wica/wica5.jpg'],
     LatLng: [ 43.6045811, -103.4213433 ],
@@ -785,7 +838,7 @@ const parkInfo = [
   },
   {
     id: '40',
-    name: 'Yellowstone National Park',
+    name: 'Yellowstone National Park, United States',
     parkCode: 'yell',
     src: ['images/yell/yell1.jpg', 'images/yell/yell2.jpg', 'images/yell/yell3.jpg', 'images/yell/yell4.jpg', 'images/yell/yell5.jpg'],
     LatLng: [ 44.427963, -110.588455 ],
@@ -794,7 +847,7 @@ const parkInfo = [
   },
   {
     id: '41',
-    name: 'Yosemite National Park',
+    name: 'Yosemite National Park, California, USA',
     parkCode: 'yose',
     src: ['images/yose/yose1.jpg', 'images/yose/yose2.jpg', 'images/yose/yose3.jpg', 'images/yose/yose4.jpg', 'images/yose/yose5.jpg'],
     LatLng: [ 37.8646393, -119.5379448 ],
@@ -803,7 +856,7 @@ const parkInfo = [
   },
   {
     id: '42',
-    name: 'Zion National Park',
+    name: 'Zion National Park, Utah, USA',
     parkCode: 'zion',
     src: ['images/zion/zion1.jpg', 'images/zion/zion2.jpg', 'images/zion/zion3.jpg', 'images/zion/zion4.jpg', 'images/zion/zion5.jpg'],
     LatLng: [ 37.2982022, -113.0263005 ],
